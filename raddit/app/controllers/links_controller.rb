992ac5c9fda9392a4ef3a05c888729194bda7b33
LibertysYarn <code@libertysyarn.com>
2014-11-13 14:25:19 -0500

    fixed undefined error in links controller

--- raddit/app/controllers/links_controller.rb
+++ raddit/app/controllers/links_controller.rb
@@ -1,6 +1,6 @@
 class LinksController < ApplicationController
-  before_filter :authenticate_user!, :except => [:index, :show]
   before_action :set_link, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :authorized_user, only: [:edit, :update, :destroy]
 
   # GET /links
@@ -26,7 +26,7 @@ class LinksController < ApplicationController
   # POST /links
   # POST /links.json
   def create
-    @link = current_user.build(link_params)
+    @link = current_user.links.build(link_params)
 
     respond_to do |format|
       if @link.save
